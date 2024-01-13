f45d0c4150cdda31cbde1deb550e55c92d42cced
tpoisonooo <khj.application@aliyun.com>
2024-01-13 15:50:45 +0800

    feat(setup.py): add whl packagine

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='HuixiangDou',
+    version='0.1',
+    packages=['package'],
+    description=  # noqa E251
+    'HuixiangDou: Overcoming Group Chat Scenarios with LLM-based Technical Assistance',  # noqa E501
+    author='tpoisonooo',
+    author_email='tpoisonooo@gmail.com',
+    url='https://github.com/internlm/huixiangdou',
+    classifiers=[
+        'License :: OSI Approved :: BSD-3 License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
