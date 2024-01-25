95f20ef51565aaa2923cd730f4dc6b6c94247ec1
tpoisonooo <khj.application@aliyun.com>
2024-01-25 19:14:48 +0800

    fix(huixiangdou): import error

--- huixiangdou/frontend/__init__.py
+++ huixiangdou/frontend/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 """IM proxy."""
 from .lark import Lark  # noqa E401
-from .lark import lark_group_send # noqa E401
+from .lark_group import send_to_lark_group # noqa E401
