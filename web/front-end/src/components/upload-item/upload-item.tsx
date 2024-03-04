a61da0b6d58eb3b1b6b2a96ec37c7bf84042ceda
Echo-minn <minn798088@gmail.com>
2024-03-04 20:38:08 +0800

    web-fe: add upload status

---
+++ web/front-end/src/components/upload-item/upload-item.tsx
@@ -0,0 +1,62 @@
+import { FC, ReactNode } from 'react';
+import { IconFont } from 'sea-lion-ui';
+import styles from './upload-item.module.less';
+
+export const enum UploadStatus {
+    init = 'init',
+    done = 'done',
+    uploading = 'uploading',
+    error = 'error',
+    removed = 'removed',
+}
+
+export interface UploadItemProps {
+    uid: string;
+    name: string;
+    status: UploadStatus;
+    progress: number;
+}
+
+const StatusColor = {
+    init: 'lightgrey',
+    done: 'green',
+    uploading: 'blue',
+    error: 'red',
+    removed: 'darkgrey'
+};
+
+const StatusIcon = {
+    init: 'icon-DocOutlined',
+    done: 'icon-CheckCircleFilled',
+    uploading: 'icon-HorizontalMoreOutlined',
+    error: 'icon-CloseCircleFilled',
+    removed: 'icon-DocOutlined'
+};
+const UploadItem: FC<UploadItemProps> = ({
+    uid, name, status, progress
+}) => {
+    return (
+        <div
+            style={{
+                border: `1px solid ${StatusColor[status]}`
+            }}
+            className={styles.uploadItem}
+            key={uid}
+        >
+            <div className={styles.icon}>
+                <IconFont color={StatusColor[status]} icon={StatusIcon[status]} />
+            </div>
+            <div>
+                <div className={styles.name}>{name}</div>
+                <div className={styles.progress}>
+                    <div
+                        className={styles.bar}
+                        style={{ width: `${progress}%` }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UploadItem;
