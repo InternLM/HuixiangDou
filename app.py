111628cb7f851a414a61d26ada5d50f087a4a2f9
tpoisonooo <khj.application@aliyun.com>
2024-03-07 14:26:36 +0800

    feat(app.py): add test code

---
+++ app.py
@@ -0,0 +1,12 @@
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch()
