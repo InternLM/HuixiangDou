0b0dd0221c7eb823d78e6796003f10d130f3e751
tpoisonooo <khj.application@aliyun.com>
2024-04-24 16:49:44 +0800

    improvement(web/tools): optimize metric tool (#254)
    * improvement(web/tools): add metric
    
    * feat(web/tools): optimize tools

--- web/tools/reconstruct_filter_annotate.py
+++ web/tools/reconstruct_filter_annotate.py
@@ -3,21 +3,7 @@
 # 3. 在已经  is_question true 中，手工标注是否需要 cr
 # * 标注规范：就看这句话是不是能构成独立的问题、不需要看其他话
 # * kimi & puyu 同时认为需要解答的内容中， puyu cr 判定的正确率
-"""在群聊场景中进行意图消解，即理解并处理群聊中用户表达的意图，是一个复杂的过程，因为它涉及到多个人、多种语境和可能的歧义。
-以下是一些在群聊场景中进行意图消解时应该注意的要点：
 
-上下文理解：理解每个发言的上下文是至关重要的。群聊中的对话往往是非线性的，可能会有多条线索同时进行。
-用户识别：识别发言者的身份可以帮助理解其发言背后的意图，尤其是在有多个参与者的情况下。
-话题跟踪：群聊中的话题可能会快速变化，跟踪当前的讨论主题对于正确理解意图至关重要。 歧义处理：群聊中的信息往往更加随意和含糊，需要能够处理语言上的歧义。
-情感分析：理解发言者的情感状态可以帮助更准确地把握其意图。 礼貌和文化差异：群聊中可能包含来自不同文化背景的人，理解和尊重这些差异对于正确消解意图很重要。
-指令识别：在群聊中，用户可能会发出指令或请求，识别这些指令并作出适当的响应是必要的。
-信息过滤：群聊中可能包含大量的信息，能够过滤掉无关信息，专注于相关的对话片段。 隐私和安全：在处理群聊数据时，保护用户隐私和数据安全是非常重要的。
-响应时间：在群聊中，及时响应可以提升用户体验，但也要注意不要过早地打断正在进行的对话。
-多任务处理：群聊可能同时涉及多个话题或任务，能够同时处理多个意图是有帮助的。
-错误处理：当理解出现偏差时，能够优雅地处理错误并从用户那里获得反馈以改进是重要的。
-用户引导：在必要时，引导用户提供更清晰的信息或指令，以帮助更好地理解其意图。 适应性：群聊的动态性要求系统能够适应不断变化的对话模式和用户行为。
-记录和回顾：有时候，为了理解当前的发言，可能需要回顾之前的聊天记录。
-"""
 import json
 import os
 import pdb
@@ -27,11 +13,11 @@ import sys
 import termios
 import time
 import tty
+import argparse
 
 from loguru import logger
 from openai import OpenAI
 from sklearn.metrics import f1_score, precision_score, recall_score
-
 
 def build_context(sender: str, query: str, window: list):
     context = ''
@@ -41,15 +27,6 @@ def build_context(sender: str, query: str, window: list):
     context += '标注问题：\n'
     context += '{}: {}\n'.format(sender, query)
     return context
-
-
-# 定义输入输出文件路径
-GROUP_ID = '20814553575'
-# 18356748488  ncnn contributor's group
-# 20814553575  openmmlab groups
-puyu_file_path = 'groups/{}@chatroom@reconstruct.txt.llm'.format(GROUP_ID)
-kimi_file_path = 'groups/{}@chatroom@reconstruct.txt.kimi'.format(GROUP_ID)
-gt_file_path = 'groups/{}@chatroom@gt.jsonl'.format(GROUP_ID)
 
 
 def kimi_is_question(query):
@@ -97,7 +74,7 @@ def kimi_is_question(query):
     return False
 
 
-def kimi_annotate():
+def kimi_annotate(puyu_file_path:str, kimi_file_path:str):
     # 读取输入文件，并逐行处理
     with open(puyu_file_path, 'r', encoding='utf-8') as input_file, open(
             kimi_file_path, 'w', encoding='utf-8') as output_file:
@@ -118,7 +95,7 @@ def kimi_annotate():
             output_file.write(json.dumps(data, ensure_ascii=False) + '\n')
 
 
-def human_annotate():
+def human_annotate(kimi_file_path:str, gt_file_path):
     # 读取输入文件，并逐行处理
     datas = []
     miss_key = 0
@@ -198,9 +175,11 @@ def human_annotate():
     print('Annotation completed. Check the output file for the results.')
 
 
-def human_check():
+def human_check(gt_file_path):
     bad = []
+    bad_text_sum = 0
     good = []
+    good_text_sum = 0
     with open(gt_file_path) as f:
         for idx, line in enumerate(f):
             data = json.loads(line)
@@ -210,19 +189,28 @@ def human_check():
 
             if data['cr_need_gt']:
                 bad.append(repr(data['text']))
+                bad_text_sum += len(data['text'])
             else:
                 good.append(repr(data['text']))
+                good_text_sum += len(data['text'])
+
 
     print(json.dumps(bad, indent=2, ensure_ascii=False))
     print(json.dumps(good, indent=2, ensure_ascii=False))
-    print('bad count {}, good count {}'.format(len(bad), len(good)))
+    print('bad count {}, avg text len {}; good count {}, avg text len {}'.format(len(bad), bad_text_sum / len(bad), len(good), good_text_sum / len(good)))
+
+    with open('groups/good.json', 'w') as f:
+        f.write(json.dumps(good, ensure_ascii=False, indent=2))
+
+    with open('groups/bad.json', 'w') as f:
+        f.write(json.dumps(bad, ensure_ascii=False, indent=2))
 
 
-def metric(dt_key='cr_need'):
+def metric(llm_type:str, filepath:str = 'groups/output.jsonl'):
     gts = []
     dts = []
 
-    with open('groups/gt.jsonl') as gt:
+    with open(filepath) as gt:
         for line in gt:
             json_obj = json.loads(line)
             if 'cr_need_gt' not in json_obj:
@@ -231,7 +219,7 @@ def metric(dt_key='cr_need'):
             cr_need_gt = json_obj['cr_need_gt']
 
             gts.append(cr_need_gt)
-            dts.append(json_obj[dt_key])
+            dts.append(json_obj['{}_cr_need'.format(llm_type)])
 
     precision = precision_score(gts, dts)
     recall = recall_score(gts, dts)
@@ -240,13 +228,206 @@ def metric(dt_key='cr_need'):
     logger.info('{} {} {}'.format(precision, recall, f1))
 
 
+def qwen_coref_res(llm_type: str, target: object):
+    model = '/workspace/models/{}'.format(llm_type)
+    client = OpenAI(
+        base_url="http://10.140.24.142:29999/v1",
+        api_key="token-abc123",
+    )
+
+    group_intro = """
+名词解释：
+open-compass/opencompass : 用于评测大型语言模型（LLM）. 它提供了完整的开源可复现的评测框架，支持大语言模型、多模态模型的一站式评测，基于分布式技术，对大参数量模型亦能实现高效评测。评测方向汇总为知识、语言、理解、推理、考试五大能力维度，整合集纳了超过70个评测数据集，合计提供了超过40万个模型评测问题，并提供长文本、安全、代码3类大模型特色技术能力评测。
+openmmlab/mmpose is an open-source toolbox for pose estimation based on PyTorch
+openmmlab/mmdeploy is an open-source deep learning model deployment toolset
+openmmlab/mmdetection is an open source object detection toolbox based on PyTorch.
+lmdeploy 是一个用于压缩、部署和服务 LLM（Large Language Model）的工具包。是一个服务端场景下，transformer 结构 LLM 部署工具，支持 GPU 服务端部署，速度有保障，支持 Tensor Parallel，多并发优化，功能全面，包括模型转换、缓存历史会话的 cache feature 等. 它还提供了 WebUI、命令行和 gRPC 客户端接入。
+茴香豆（HuixiangDou）是一个基于 LLM 的群聊知识助手。设计拒答、响应两阶段 pipeline 应对群聊场景，解答问题同时不会消息泛滥。
+xtuner is an efficient, flexible and full-featured toolkit for fine-tuning large models.
+mmyolo : YOLO series toolbox and benchmark. Implemented RTMDet, RTMDet-Rotated,YOLOv5, YOLOv6, YOLOv7, YOLOv8,YOLOX, PPYOLOE, etc.
+ncnn is a high-performance neural network inference framework optimized for the mobile platform
 """
-1. 用 kimi 二次标注，基于 `reconstruct_wechat_group.py is_question` 筛选
-2. kimi & LLM 同时认为是问题的，过滤太短的，人工标注
-3. 检查一遍
-4. 基于人工 GT 计算 LLM 的精度
-"""
-# kimi_annotate()
-# human_annotate()
-# human_check()
-metric()
+
+    window = target['cr_window']
+    # logger.debug('input window {}'.format(window))
+    name_map = dict()
+    name_int = ord('A')
+    # chr(start_ascii + i)
+    format_history = []
+    for item in window:
+        sender = item['sender']
+        if sender not in name_map:
+            name_map[sender] = chr(name_int)
+            name_int += 1
+
+        format_history.append({
+            'username': name_map[sender],
+            'content': item['text']
+        })
+
+    target_sender = target['sender']
+    if target_sender not in name_map:
+        name_map[target_sender] = chr(name_int)
+        name_int += 1
+
+    target_str = json.dumps(
+        {
+            'username': name_map[target_sender],
+            'content': target['text']
+        },
+        indent=2,
+        ensure_ascii=False)
+
+    BASE_PROMPT_TEMPLATE = '''群聊场景中“这”、“它”、“哪”等代词需要查看上下文和其他用户的回复才能确定具体指什么，请完成群聊场景代词替换任务。
+
+以下是历史对话，可能有多个人的发言：
+{}
+
+输入内容：
+"{}"'''
+    prompt_base = BASE_PROMPT_TEMPLATE.format(
+        json.dumps(format_history, ensure_ascii=False),
+        target_str)
+
+    prompt = '{}\n输入内容中的 content 信息是否完整，是否需要从历史对话中提取代词或宾语来替代 content 中的一部分词汇？ A：不需要提取，信息完整  B：需要  C：不知道 \n一步步分析，首先历史消息包含哪些话题；其次哪个话题与问题最相关；如果都不相关就不提取。 '.format(prompt_base)
+
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    need_cr = completion.choices[0].message.content.lower()
+    need_cr = need_cr.strip()
+    logger.debug('{} {}'.format(prompt, need_cr))
+
+    response = ''
+
+    prompt = """请判断用户意图，这位用户在做单选题，单选题答案有 3 个， A：不需要提取，信息完整  B：需要  C：不知道。
+用户输入：
+{}
+
+用户的答案是？不要解释，直接给 ABC 选项结果。
+""".format(need_cr)
+
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    need_cr = completion.choices[0].message.content.lower()
+    need_cr = need_cr.strip()
+
+    logger.warning('final choose {}'.format(need_cr))
+
+    if need_cr.startswith('a') or need_cr == '不需要' or '因此不需要' in need_cr or 'a：不需要' in need_cr or '不需要进行指代消解' in need_cr or '选项 a' in need_cr:
+        return '', 'no'
+    elif need_cr.startswith('b') or need_cr == '需要' or '因此需要' in need_cr or '因此选择b' in need_cr or '需要进行指代消解' in need_cr or '需要指代消解' in need_cr or 'b：需要' in need_cr:
+        prompt = '{}\n指代消解输入内容中 content 后的文本是？直接返回消解后的完整文本不要解释原因；直接返回最终结果不要解释过程。'.format(
+            prompt_base)
+    
+        completion = client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+        response = completion.choices[0].message.content.lower()
+    elif need_cr.startswith('c') or '不知道' in need_cr:
+        return '', 'unknown'
+    else:
+        return '', 'exception {}'.format(need_cr)
+
+
+    keywords = ['指代消解后的文本是：', '指代消解后是：', '指代消解后：', '指代消解后的文本为：']
+    for keyword in keywords:
+        if keyword in response:
+            response = response.split(keyword)[-1]
+    response = response.strip()
+    if response.startswith('"') and response.endswith('"'):
+        response = response[1:-1]
+    logger.warning('coref response {}'.format(response))
+    return response, 'yes'
+
+
+def llm_annotate(llm_type: str, input_filepath:str = 'groups/input.jsonl', output_filepath = 'groups/output.jsonl'):
+    idx = 0
+    with open(input_filepath) as fin, open(output_filepath, 'a') as fout:
+        for line in fin:
+            json_obj = json.loads(line)
+            if not json_obj['is_question']:
+                continue
+            if not json_obj['kimi_is_question']:
+                continue
+
+            idx += 1
+
+            if 'qwen' in llm_type.lower():
+                cr_text, state = qwen_coref_res(llm_type=llm_type, target=json_obj)
+            json_obj['{}_cr_text'.format(llm_type)] = cr_text
+            json_obj['{}_cr_need'.format(llm_type)] = state
+
+            json_text = json.dumps(json_obj, ensure_ascii=False)
+            fout.write(json_text)
+            fout.write('\n')
+        
+
+def parse_args():
+    """Parse args."""
+    parser = argparse.ArgumentParser(description='Annotate and metric LLM with CR task.')
+    parser.add_argument('--group-id',
+                        type=str,
+                        default='20814553575',
+                        help='Group ID')
+    parser.add_argument(
+        '--input',
+        type=str,
+        default='/home/khj/github/huixiangdou/tests/history_recv_send.txt',
+        help='Raw input messages.')
+    parser.add_argument(
+        '--action',
+        type=str,
+        # default='split',
+        default='metric',
+        help=
+        '"annotate"): manually annotate query; "metric"): test with LLM and metric'
+    )
+    parser.add_argument(
+        '--llm-type',
+        type=str,
+        # default='split',
+        # default='qwen-moe-2.7B-chat',
+        default='Qwen1.5-14B-Chat',
+        help='LLM type, use qwen moe by default.'
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    # 定义输入输出文件路径
+    # 18356748488  ncnn contributor's group
+    # 20814553575  openmmlab groups
+
+    if args.action == 'annotate':
+        """
+        1. 用 kimi 二次标注，基于 `reconstruct_wechat_group.py is_question` 筛选
+        2. kimi & LLM 同时认为是问题的，过滤太短的，人工标注
+        3. 检查一遍
+        4. 基于人工 GT 计算 LLM 的精度
+        """
+        group_id = args.group_id
+        puyu_file_path = 'groups/{}@chatroom@reconstruct.txt.llm'.format(group_id)
+        kimi_file_path = 'groups/{}@chatroom@reconstruct.txt.kimi'.format(group_id)
+        gt_file_path = 'groups/{}@chatroom@gt.jsonl'.format(group_id)
+        kimi_annotate()
+        human_annotate()
+    elif args.action == 'check':
+        human_check('groups/input.jsonl')
+    elif args.action == 'metric':
+        output_filepath = 'groups/qwen32B.jsonl'
+        llm_annotate(llm_type=args.llm_type, output_filepath=output_filepath)
+        metric(llm_type=args.llm_type, filepath=output_filepath)
