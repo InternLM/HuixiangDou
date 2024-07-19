56e8bf63cc4c5b5667f031fb5d3096f27bb57ced
tpoisonooo <khj.application@aliyun.com>
2024-07-19 15:31:28 +0800

    bugfix(wechat.py): fix crash

--- huixiangdou/frontend/wechat.py
+++ huixiangdou/frontend/wechat.py
@@ -255,13 +255,16 @@ class Message:
 
 def empty_list():
     return []
-    
+
 @dataclass
 class Talk:
     query: str
     reply: str = ''
-    refs: list = field(default_factory=empty_list)
+    refs: tuple = ()
     now: int = time.time()
+
+def convert_talk_to_dict(talk: Talk):
+    return {'query': talk.query, 'reply': talk.reply, 'refs': talk.refs, 'now': talk.now}
 
 def convert_history_to_tuple(history: List[Talk]):
     return [(item.query, item.reply, item.refs, item.now) for item in history]
@@ -281,16 +284,18 @@ class User:
 
     def __str__(self):
         obj = {
-            'history': self.history,
+            'history': [],
             'last_msg_time': self.last_msg_time,
             'last_process_time': self.last_process_time,
             '_id': self._id
         }
+        for item in self.history:
+            obj['history'].append(convert_talk_to_dict(item))
         return json.dumps(obj, indent=2, ensure_ascii=False)
 
     def feed(self, msg: Message):
         if msg.type in ['url', 'image']:
-            talk = Talk(query=msg.query, refs=[msg.url])
+            talk = Talk(query=msg.query, refs=(msg.url))
             self.history.append(talk)
         else:
             talk = Talk(query=msg.query)
@@ -327,9 +332,9 @@ class User:
 
     def update_history(self, query, reply, refs):
         if type(refs) is list:
-            talk = Talk(query=query, reply=reply, refs=refs)
+            talk = Talk(query=query, reply=reply, refs=tuple(refs))
         else:
-            talk = Talk(query=query, reply=reply, refs=[refs])
+            talk = Talk(query=query, reply=reply, refs=(refs))
         self.history[-1] = talk
         self.last_process_time = time.time()
 
