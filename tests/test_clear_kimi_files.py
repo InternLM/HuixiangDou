befa9f14d0bcfda0f252f0f650e4df02061be415
tpoisonooo <khj.application@aliyun.com>
2024-07-19 16:10:09 +0800

    examples(tests): remove kimi files

---
+++ tests/test_clear_kimi_files.py
@@ -0,0 +1,10 @@
+from openai import OpenAI
+import os
+import pdb
+from tqdm import tqdm
+
+client = OpenAI(api_key=os.getenv('MOONSHOT_API_KEY'), base_url='https://api.moonshot.cn/v1')
+file_list = client.files.list()
+for file in tqdm(file_list.data):
+    client.files.delete(file_id = file.id)
+    print(file)
