f75bea5293cdc0c408776b39bcc043082cee0b0c
Echo-minn <minn798088@gmail.com>
2024-07-25 10:34:20 +0800

    web-fe:[feat] mod proxy

--- web/front-end/scripts/proxy.ts
+++ web/front-end/scripts/proxy.ts
@@ -1,7 +1,7 @@
 // https://github.com/http-party/node-http-proxy#options
 const ProxyConfig = {
     '/api': {
-        target: 'https://p-172_dot_31_dot_0_dot_170_colon_18443.openxlab.space',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
         rewrite: path => {
