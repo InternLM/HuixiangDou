1595adb05a1eec17524158faf3c035663973c52d
tpoisonooo <khj.application@aliyun.com>
2024-08-06 15:03:45 +0800

    Update llm_server_hybrid.py

--- huixiangdou/service/llm_server_hybrid.py
+++ huixiangdou/service/llm_server_hybrid.py
@@ -155,7 +155,7 @@ class InferenceWrapper:
         elif type(self.model).__name__ == 'InternLM2ForCausalLM':
 
             if '请仔细阅读以上内容，判断句子是否是个有主题的疑问句，结果用 0～10 表示。直接提供得分不要解释。' in prompt:
-                prompt = '你是一个语言专家，擅长分析语句并打分，你会忽略不认识的名词，只关心句子结构本身。\n' + prompt
+                prompt = '你是一个语言专家，擅长分析语句并打分。\n' + prompt
 
             output_text, _ = self.model.chat(self.tokenizer, prompt, history, top_k=1, do_sample=False)
         else:
